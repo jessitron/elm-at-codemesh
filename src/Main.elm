adeb803a7245f39b65df22b1f88f566dd7b0bc21
Jessica Kerr <jessitron@stripe.com>
2016-11-04 05:36:08 -0500

    applied editor

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,17 @@
 module Main exposing (main)
 
-import Html
+import Html exposing (Html)
+import Html.Attributes exposing (style)
 
 
+main : Html Never
 main =
-    Html.text "Hello World"
+    Html.div []
+        [ Html.main' []
+            [ Html.canvas
+                [ style [ ( "backgroundImage", "url(elmview.png)" ) ]
+                ]
+                []
+            ]
+        , Html.aside [] [ Html.text "Hello World" ]
+        ]
