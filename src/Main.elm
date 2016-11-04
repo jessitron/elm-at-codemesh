84aa1867070b9b299395a127d9c36093f475ff8b
Jessica Kerr <jessitron@stripe.com>
2016-11-04 05:38:54 -0500

    applied editor

--- src/Main.elm
+++ src/Main.elm
@@ -2,16 +2,60 @@ module Main exposing (main)
 
 import Html exposing (Html)
 import Html.Attributes exposing (style)
+import Html.App
 
 
-main : Html Never
+main : Program Never
 main =
+    Html.App.beginnerProgram
+        { model = model
+        , update = update
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { diagramUrl : String, message : String }
+
+
+model : Model
+model =
+    { diagramUrl = "elmview.png"
+    , message = "Hello World"
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Noop
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Noop ->
+            model
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
     Html.div []
         [ Html.main' []
             [ Html.canvas
-                [ style [ ( "backgroundImage", "url(elmview.png)" ) ]
+                [ style [ ( "backgroundImage", "url(" ++ model.diagramUrl ++ ")" ) ]
                 ]
                 []
             ]
-        , Html.aside [] [ Html.text "Hello World" ]
+        , Html.aside [] [ Html.text model.message ]
         ]
